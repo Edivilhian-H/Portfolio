088d14ab79a2d438c1f60819ff1dbf01bfbe60df
Edivilhian-Hipolito <97316805+Edivilhian-H@users.noreply.github.com>
2026-04-21 11:08:21 -0300

    finalizando proejto

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
+
+  const img = document.querySelector("#profileImg")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avater-light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
 }
